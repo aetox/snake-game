88e041bf438f2c111da8085fdb925bb068aa85c9
aetox <dimitri.kapolas04@gmail.com>
2024-06-13 14:19:14 +0200

    chore: Update package.json with new dependencies and scripts

--- tests/game.test.js
+++ tests/game.test.js
@@ -1,8 +1,13 @@
 import { Game } from '../src/game.js';
 
+beforeEach(() => {
+    document.body.innerHTML = '<div id="game-board"></div><div id="message"></div>';
+});
+
 test('should initialize game correctly', () => {
     const game = new Game();
     game.init();
     expect(game.players.length).toBe(2);
     expect(game.boardSize).toBe(50);
+    expect(document.getElementById('game-board').children.length).toBe(50);
 });
